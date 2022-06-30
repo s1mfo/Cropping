e5b5052d283e85f3216e029bd7c22d87bd0f09b7
Jeongbo Sim <imfo3842@gmail.com>
2022-06-30 22:09:47 +1000

    Second

--- Cropping.py
+++ Cropping.py
@@ -1,46 +1,54 @@
-# pynput 이란 키보드와 마우스를 제어할 수 있는 파이썬 라이브러리.
-# Listener 등록을 통해 키보드와 마우스로부터 들어오는 값을 가져올 수 있다.
+import pyautogui
+import tkinter as tk
 from pynput.mouse import Listener
 import keyboard
 from PIL import Image
-import pyautogui
 import os
 
+def cropping(event):   
+    coord = []
+    # saving screenshot image to D drive
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save('D:/myscreenshot.jpg')
+    im = Image.open(r'D:/myscreenshot.jpg')
+    # getting mouse coordinates when mouse click.
+    def click(x,y,buton,pressed):
+        if pressed:
+            x = int(x)
+            y = int(y)
+            coord.append(x)
+            coord.append(y)
+            if len(coord) == 4:
+                return False
+    with Listener(on_click = click) as listener:
+        listener.join()
+        
+    left = coord[0]
+    top = coord[1]
+    right = coord[2]
+    bottom = coord[3]
+    im1 = im.crop((left,top,right,bottom))
+    os.remove('D:/myscreenshot.jpg')
+    os.startfile('D:/')
+    im1.show()
+    im1.save('D:/cropped.jpg')
 
-coord = []
+root = tk.Tk()
+root.title('BetterWork')
+canvas1 = tk.Canvas(root, width = 400, height = 100)
+canvas1.pack()
 
-# saving screenshot image to D drive
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save('D:/myscreenshot.jpg')
+def crop():
+    if onButton['text'] == 'OFF':
+        onButton['text'] = 'ON'
+        onButton['bg'] = 'blue'
+        root.bind('<Shift-Z>', cropping)
+    else:
+        root.unbind('<Shift-Z>')
+        onButton['text'] = 'OFF'
+        onButton['bg'] = 'red'
+        
+onButton = tk.Button(text = 'OFF', command = crop, bg='red', fg='white', font=15, width=25)
+canvas1.create_window(200, 50, window = onButton)
 
-
-im = Image.open(r'D:/myscreenshot.jpg')
-
-# getting mouse coordinates when mouse click.
-def click(x,y,buton,pressed):
-    if pressed:
-        x = int(x)
-        y = int(y)
-        coord.append(x)
-        coord.append(y)
-
-        if len(coord) == 4:
-            return False
-
-with Listener(on_click = click) as Listener:
-    Listener.join()
-
-left = coord[0]
-top = coord[1]
-right = coord[2]
-bottom = coord[3]
-
-im1 = im.crop((left,top,right,bottom))
-
-os.remove('D:/myscreenshot.jpg')
-os.startfile('D:/')
-im1.show()
-im1.save('D:/cropped.jpg')
-
-
-
+root.mainloop()
